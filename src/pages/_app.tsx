cba3dcbb219fb1cfad3d426781bca0bd809aaab8
alok-dqot <alok@dqotsolutions.com>
2024-07-19 12:19:56 +0530

    intialize the drag free carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '@/styles/Home.module.css';
+import '@/styles/embla.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
